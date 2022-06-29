1a989bcd7d51636e934f6e2a943c1d4181e356ec
EsdrasPedro <esdraspedro@hotmail.com.br>
2022-06-28 21:58:13 -0300

    :heavy_plus_sign: build: Adição de dependências do projeto #6

---
+++ Mundo_Organico/src/test/java/java/start/projetc/mundo_organico/MundoOrganicoApplicationTests.java
@@ -0,0 +1,13 @@
+package java.start.projetc.mundo_organico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundoOrganicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
