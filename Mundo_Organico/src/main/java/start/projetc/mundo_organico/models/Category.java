fb2c6be68fa5d1ffa366ddb097204aca1abdbe8f
Ygor Alves <103756299+dev-ygor@users.noreply.github.com>
2022-06-30 12:53:19 -0300

    :sparkles: feat: Criado model de Categoria e Tipo #11

---
+++ Mundo_Organico/src/main/java/start/projetc/mundo_organico/models/Category.java
@@ -0,0 +1,79 @@
+package start.projetc.mundo_organico.models;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "category")
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "category")
+	private Set<Type> types = new HashSet<>();
+	
+	public Category() {
+	}
+
+	public Category(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+	public Set<Type> getTypes() {
+		return types;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
+
