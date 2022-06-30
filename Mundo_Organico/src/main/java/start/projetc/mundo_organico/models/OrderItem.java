46ef744d79475dff9866d98dd6eaadca944a38c8
Ygor Alves <103756299+dev-ygor@users.noreply.github.com>
2022-06-30 13:58:01 -0300

    :sparkles: feat: Criação model Itens de pedido #30

---
+++ Mundo_Organico/src/main/java/start/projetc/mundo_organico/models/OrderItem.java
@@ -0,0 +1,91 @@
+package start.projetc.mundo_organico.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import start.projetc.mundo_organico.models.PK.OrderItemPK;
+
+@Entity
+@Table(name = "orderItem")
+public class OrderItem implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private OrderItemPK id = new OrderItemPK();
+	
+	private Double value;
+	private Integer quantity;
+	
+	public OrderItem() {
+	}
+
+	public OrderItem(Order order, Product product, Double value, Integer quantity) {
+		super();
+		id.setOrder(order);
+		id.setProduct(product);
+		this.value = value;
+		this.quantity = quantity;
+	}
+	
+	@JsonIgnore
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	public double getSubTotal() {
+		return value * quantity;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
+
