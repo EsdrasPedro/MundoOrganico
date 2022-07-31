b5d485af329a7c05600b5930ea23ce94328cfc2f
EsdrasPedro <esdraspedro@hotmail.com.br>
2022-07-31 15:29:34 -0300

    :sparkles: feat: Adição de function no JS #47
    Função que manda o valor do contador para um input

--- Mundo_Organico/src/main/resources/static/js/script.js
+++ Mundo_Organico/src/main/resources/static/js/script.js
@@ -3,16 +3,25 @@ var quantidade = 0;
 
 function increment() {
     quantidade += 1;
+    mandar();
     contador.innerHTML = quantidade;
 }
 
 function decrement() {
     if(quantidade == 0){
         quantidade = 0;
+        mandar();
         contador.innerHTML = quantidade;
         
     } else {
         quantidade -= 1;
+        mandar();
         contador.innerHTML = quantidade;
     }
+}
+
+function mandar() {
+    var myelement = document.querySelector('input[name="quant"]');
+    myelement.value = quantidade;
+    console.log(myelement.value)
 }
